f58dba8db3d51b192aae75b70475cb686247b61a
Pakornpong Bird <70741555+birdpakornpong@users.noreply.github.com>
2022-11-19 03:58:32 +0700

    main learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,75 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
-import './screen/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigator',
-      home: HomeScreen(),
-    );
+        title: 'Navigator',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Navigator',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Bird Flutter"),
+          ),
+          body: Center(
+            child: Column(
+                // กำหนด position children in column
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    '$_counter',
+                  ),
+                  const Text(
+                    'Hello bird',
+                  ),
+                  const Image(
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2022/10/21/08/39/cat-7536508_1280.jpg")),
+                  const Image(
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2022/11/02/04/07/deer-7563934_1280.jpg"),
+                  )
+                ]),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ));
   }
 }
