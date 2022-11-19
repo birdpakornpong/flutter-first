c0465fe04095c94ac932d148882847c64c0f314a
Pakornpong Bird <70741555+birdpakornpong@users.noreply.github.com>
2022-11-20 02:16:43 +0700

    async await function

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,23 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'package:flutter/material.dart';
-import './components/FoodMenu.dart';
-
-void main() {
-  runApp(const MyApp());
+void main() async {
+  print("กำลังทำงาน รอ 10 วิ");
+  print(await loginUserFuture());
+  print("ทำงานต่อไป");
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Navigator',
-        theme: ThemeData(
-          primarySwatch: Colors.pink,
-        ),
-        home: const MyHomePage());
-  }
+String loginUser() {
+  var user = getUserFromDatabase();
+  return user;
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+String getUserFromDatabase() {
+  return "kongruksiam";
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("บัญชีของฉัน"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(children: [
-            MoneyBox("ยอดคงเหลือ", 120000, Colors.lightBlue, 120),
-            const SizedBox(
-              height: 5,
-            ),
-            MoneyBox("ยอดคงเหลือ", 120000, Colors.lightBlue, 120),
-            MoneyBox("ยอดคงเหลือ", 120000, Colors.lightBlue, 120),
-          ]),
-        ));
-  }
+Future<String> loginUserFuture() async {
+  var user = await getUserFromDatabaseFuture();
+  return user;
+}
+
+Future<String> getUserFromDatabaseFuture() {
+  return Future.delayed(const Duration(seconds: 10), () => "bird pakornpong");
 }
---
+++ lib/main201122widgetcustom.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import './components/FoodMenu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Navigator',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    print('เรียกใช้งาน init State');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("บัญชีของฉัน"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            MoneyBox("ยอดคงเหลือ", 120000, Colors.lightBlue, 120),
+            const SizedBox(
+              height: 5,
+            ),
+            MoneyBox("ยอดคงเหลือ", 120000, Colors.lightBlue, 120),
+            MoneyBox("ยอดคงเหลือ", 120000, Colors.lightBlue, 120),
+          ]),
+        ));
+  }
+}
