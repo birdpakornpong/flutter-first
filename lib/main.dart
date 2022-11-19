41290a898733931cd5b21a52364b85ea7529f756
Pakornpong Bird <70741555+birdpakornpong@users.noreply.github.com>
2022-11-20 03:42:30 +0700

    get api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http; // as http ตั้งชื่อเพื่อเอาไปใช้
 
 void main() {
   runApp(const MyApp());
@@ -26,11 +26,32 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class UserAgentClient extends http.BaseClient {
+  final String userAgent;
+  final http.Client _inner = http.Client();
+
+  UserAgentClient(this.userAgent);
+
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['apikey'] = userAgent;
+    return _inner.send(request);
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
     print('เรียกใช้งาน init State');
+    getExchangeRate();
+  }
+
+  Future<void> getExchangeRate() async {
+    var url = Uri.https("api.apilayer.com", "/exchangerates_data/latest");
+    var https = UserAgentClient("1UpDuDknADWynwDbfrentuTRjOgweddB");
+
+    var response = await https.get(url);
+    print(response.body);
   }
 
   @override
